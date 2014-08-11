6f28c0520d418b84438dba13ec3db4a5dd20c711
madHamish42 <pe_di@gmx.de>
2014-08-11 19:12:13 +0200

    after ex5

--- src/one_function_to_rule_them_all.clj
+++ src/one_function_to_rule_them_all.clj
@@ -14,11 +14,16 @@
 (defn my-interpose [x a-seq]
   (rest (reduce #(conj %1 x %2) [] a-seq)))
 
-(defn my-count [a-seq]
-  :-)
+(defn my-count2 [a-seq]
+  (reduce #(if (not %2)
+             %2
+             (inc %1)) 0 a-seq))
+
+(defn my-count2 [a-seq]
+  (reduce (fn [count _] (inc count)) 0 a-seq))
 
 (defn my-reverse [a-seq]
-  [:-])
+  (reduce #(cons %2 %1) () a-seq))
 
 (defn min-max-element [a-seq]
   [:-])
@@ -47,4 +52,5 @@
 (defn my-map [f a-seq]
   [:-])
 
-
+(str-cat2 ["1"])
+(my-reverse [])  
