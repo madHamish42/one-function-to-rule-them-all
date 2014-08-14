09a0a44507932ce8ddb84c252fa60b94f010fe78
madHamish42 <pe_di@gmx.de>
2014-08-14 18:35:03 +0200

    after ex7a

--- src/one_function_to_rule_them_all.clj
+++ src/one_function_to_rule_them_all.clj
@@ -19,17 +19,28 @@
              %2
              (inc %1)) 0 a-seq))
 
-(defn my-count2 [a-seq]
+(defn my-count [a-seq]
   (reduce (fn [count _] (inc count)) 0 a-seq))
 
 (defn my-reverse [a-seq]
   (reduce #(cons %2 %1) () a-seq))
 
 (defn min-max-element [a-seq]
-  [:-])
+  (reduce (fn [min-max num]
+            (cond
+             (empty? min-max) [num num]
+             (< num (get min-max 0)) (assoc min-max 0 num)
+             (> num (get min-max 1)) (assoc min-max 1 num)
+             :else min-max))
+          [] a-seq))
 
 (defn insert [sorted-seq n]
-  [:-])
+  (loop [sorted []
+         rst sorted-seq]
+    (cond
+     (empty? rst) (seq (conj sorted n))
+     (< (first rst) n) (recur (conj sorted (first rst)) (rest rst))
+     :else (concat sorted [n] rst))))
 
 (defn insertion-sort [a-seq]
   [:-])
@@ -52,5 +63,5 @@
 (defn my-map [f a-seq]
   [:-])
 
-(str-cat2 ["1"])
-(my-reverse [])  
+
+(insert [1 3] 2)  
